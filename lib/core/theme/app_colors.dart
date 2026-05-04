b37be8df28fdab43f8c767b9f6e29f55f00765a0
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-05-04 22:42:33 +0545

    Setup project,app themes,colors and text styles

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2D5BFF);
+  static const Color secondary = Color(0xFF00C853);
+  static const Color tertiary = Color(0xFFF59E0B);
+  static const Color neutral = Color(0xFF1E293B);
+
+  static const Color background = Color(0xFFF8FAFC);
+  static const Color white = Colors.white;
+}
