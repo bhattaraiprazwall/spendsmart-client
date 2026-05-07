0a8464e7770d8d5e4f81c5555acd964df2a19937
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-05-07 23:06:57 +0545

    Created custom textfield widget

--- lib/core/widgets/custom_textfield.dart
+++ lib/core/widgets/custom_textfield.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class CustomTextfield extends StatefulWidget {
+  final TextEditingController controller;
+  final String hint;
+  final TextInputType keyboardType;
+  final bool isPassword;
+  const CustomTextfield({
+    super.key,
+    required this.controller,
+    required this.hint,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+  });
+
+  @override
+  State<CustomTextfield> createState() => _CustomTextfieldState();
+}
+
+class _CustomTextfieldState extends State<CustomTextfield> {
+  bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      obscureText: widget.isPassword && _obscurePassword,
+      decoration: InputDecoration(
+        hintText: widget.hint,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _obscurePassword = !_obscurePassword;
+                  });
+                },
+                icon: Icon(
+                  _obscurePassword
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                ),
+              )
+            : null,
+      ),
+    );
   }
 }
