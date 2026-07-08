880ec33b6f9d83beb2a42c5c62d9bd4a6cd90bda
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-07-08 23:47:00 +0545

    Updated the password field

--- lib/features/profile/data/repositories/profile_repository.dart
+++ lib/features/profile/data/repositories/profile_repository.dart
@@ -6,4 +6,24 @@ class ProfileRepository {
   Future<Map<String, dynamic>> getProfile(String idToken) async {
     return await _profileService.getProfile(idToken);
   }
+
+  Future<Map<String, dynamic>> updateProfile(
+    String idToken, {
+    String? name,
+    String? avatarUrl,
+    String? password,
+    String? currency,
+    String? theme,
+    String? language,
+  }) async {
+    return await _profileService.updateProfile(
+      idToken,
+      name: name,
+      avatarUrl: avatarUrl,
+      password:password,
+      currency: currency,
+      theme: theme,
+      language: language,
+    );
+  }
 }
