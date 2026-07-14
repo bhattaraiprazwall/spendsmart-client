e1cae8afabb1906dbfdbc5710b0c8134571d19c9
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-07-14 23:12:16 +0545

    Added the method to fetch the default and custom categories

---
+++ lib/features/category/data/repositories/category_repository.dart
@@ -0,0 +1,24 @@
+import 'package:spendsmart/features/category/data/models/category_model.dart';
+import 'package:spendsmart/features/category/data/services/category_service.dart';
+
+class CategoryRepository {
+  final CategoryService _categoryService = CategoryService();
+
+  Future<List<CategoryModel>> getCategories(String idToken) async {
+    return await _categoryService.getCategories(idToken);
+  }
+
+  Future<CategoryModel> createCategory(
+    String idToken, {
+    required String name,
+    required String icon,
+    required String color,
+  }) async {
+    return await _categoryService.createCategory(
+      idToken,
+      name: name,
+      icon: icon,
+      color: color,
+    );
+  }
+}
