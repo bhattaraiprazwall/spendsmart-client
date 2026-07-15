4e2a9984625e26b3574b736418125b22e1c1913f
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-07-15 22:48:41 +0545

    category icon and color constant

---
+++ lib/core/constants/category_constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final List<IconData> icons = [
+  Icons.pets,
+  Icons.shopping_cart,
+  Icons.restaurant,
+  Icons.local_gas_station,
+  Icons.home,
+  Icons.flight,
+  Icons.medical_services,
+  Icons.checkroom,
+  Icons.video_library,
+  Icons.more_horiz,
+];
+
+
+final List<Color> colors = [
+    Colors.blue,
+    Colors.green,
+    Color(0xFF8B6914),
+    Colors.red,
+    Colors.purple,
+    Colors.pink,
+  ];
